023b3c5069e229994c900d85d47462b0f0dbd594
cksk-jp <carlson.ksk@gmail.com>
2023-12-19 16:45:40 -0500

    exercise completed

--- convert.py
+++ convert.py
@@ -16,6 +16,14 @@ def convert_temp(unit_in, unit_out, temp):
     """
 
     # YOUR CODE HERE
+    if (unit_in == 'c' and unit_out =='f'):
+        return (temp * 9/5) + 32
+    elif (unit_in == 'f' and unit_out =='c'):
+        return (temp - 32) * 5/9
+    elif (unit_in != 'c'):
+        return f'Invalid unit {unit_in}'
+    else:
+        return f'invalid unit {unit_out}'
 
 
 print("c", "f", 0, convert_temp("c", "f", 0), "should be 32.0")
